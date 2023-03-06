5878a5c6a39222d6fc1889a2bf478c04d4da4048
esJohan <imjohanjjcc@gmail.com>
2023-03-06 01:30:55 -0600

    ffl5

--- main.go
+++ main.go
@@ -6,8 +6,9 @@ type person struct{
 	secondName string 
 	fis []string
 }
+
 func main(){
-	ej2()
+	ej4()
 }
 
 func ej1(){
@@ -29,6 +30,7 @@ func ej1(){
 }
 
 func ej2(){
+	//Here you got a  Map that has a Struct named person and inside that struct there is a slice
 	person1 := person{
 		firstName: "Johan",
 		secondName: "Josue",
@@ -39,17 +41,63 @@ func ej2(){
 		secondName: "Cole",
 		fis: []string{"Red","Blue","Grey"},
 	}
-
 	m := map[string]person{
 		person1.firstName:person1,
 		person2.firstName:person2,
 	}
-	fmt.Println(m)
-	fmt.Println("Now in the foor loop")
+	// fmt.Println(m)
+	// fmt.Println("Now in the foor loop")
 	for i,k := range m{
-		fmt.Printf("The person is:%v\n",k)
-		for x, z:= range i{
-			fmt.Printf("This is the value:%v\t This is the position:%v\n ",z,x)
+		fmt.Printf("The person is:%v\n",i)
+		for x, z:= range k.fis{
+			fmt.Printf(" This is the position:%v\t This is the value:%v\n ",x,z)
 		}
+	}
+}
+func ej3(){
+	//using a compose literal create truck and sedan
+	type vehicle struct{
+		doors int
+		color string 
+	}
+	type sedan struct{
+		 v vehicle
+		 luxury bool
+	}
+	type truck struct{
+		v vehicle
+		fourWheel bool
+	}
+	corolla := sedan{
+		v: vehicle{4, "Red"},
+		luxury: true,
+	}
+	frontier:= truck{
+		v: vehicle{4, "Grey"},
+		fourWheel: true,
+	}
+	fmt.Println(corolla)
+	fmt.Println(frontier)
+
+}
+func ej4(){
+	s := struct{
+		name string
+		friends map[string]string
+		Colors []string
+	}{
+		name: "Johan",
+		friends:map[string]string{
+				"Sergio": "Es su mejor amigo",
+				"Pedro": "No es su mejor amigo",
+		} ,
+		Colors: []string{"Red","Blue","Grey"},
+	}
+	fmt.Println("Name:",s.name)
+	for k, row := range s.friends{
+		fmt.Printf("Key:%v\tContent:%v\n",k,row)
+	}
+	for i,k := range s.Colors{
+		fmt.Println("Position Index:",i, "Content inside Index:",k)
 	}
 }
